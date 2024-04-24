8addbd277e83ad398ed8b94e78ec261ca5f59810
ennasiri <149584371+ennasiri@users.noreply.github.com>
2024-04-24 02:37:32 -0700

    Delete modele directory

--- modele/hebergement/HebergDao.java
+++
@@ -1,6 +0,0 @@
-package modele.hebergement;
-
-public interface HebergDao {
-    //Pour ajouter un hebergement à la base de données
-    public boolean addHebergement(Hebergement hebergement) throws ClassNotFoundException;
-}
